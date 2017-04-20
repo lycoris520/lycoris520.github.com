64d81017be67b95ef9595bea6a53c948bdd54a15
Lycoris <yangxuemei@juwang.cn>
2017-04-20 16:15:12 +0800

    测试

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+gulp.task('default',function(){
+    //place code for your default task here
+    console.log('lycoris test');
+})
+gulp.task('test',function(){
+    //place code for your default task here
+    console.log('test task ');
+})
