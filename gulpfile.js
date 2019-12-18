c54f070907c7de3b3d368a84c1c40fdbb69f4df9
杨子 <526839572@qq.com>
2019-12-18 10:42:55 +0800

    更改为后台项目模板

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-gulp.task('default',function(){
-    //place code for your default task here
-    console.log('lycoris test');
-})
-gulp.task('test',function(){
-    //place code for your default task here
-    console.log('test task ');
-})
